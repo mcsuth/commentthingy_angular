26955626198aaa931960c58bc5ce47c14e0dec7e
mcsuth <seimith@gmail.com>
2014-11-08 13:27:24 -0800

    Icons

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 var app = angular.module('app', []);
 
 app.controller('mainController', ['$scope', '$http', function($scope, $http) {
-  this.user = "Smith Suth";
+  this.user = {name: "Smith Suth", img: 'img/fem.png'};
   this.feeddata = [
-    {username: 'Anderson Von Quigley', link: 'Bacon ipsum dolor amet drumstick jerky venison tri-tip meatloaf swine leberkas bresaola frankfurter. Chicken ham hock ball tip leberkas corned beef. Frankfurter hamburger pig pastrami pork chop leberkas, filet mignon venison porchetta pancetta kielbasa tongue. ', 
-    comments: [{username: 'Ryan Williams', comment:'How do I register?'}]},
-    {username: 'Fred Bradley', link: 'Doner capicola chuck shoulder prosciutto tongue. Strip steak drumstick biltong chuck sirloin pancetta cow shank turducken tenderloin venison. Shankle t-bone bacon drumstick shoulder jowl frankfurter pancetta sausage tri-tip short loin.', comments: [{username: 'Ryan Williams', comment:'Can I do this?'}, {username: 'Bryan Lee', comment:'Yeah, man!'}, {username: 'Jackie Blue', comment:'Nope.'}]},
-    {username: 'Anderson Von Quigley', link: 'Drumstick bacon beef sirloin, tri-tip porchetta pastrami.', comments: []},
-    {username: 'Anderson Von Quigley', link: 'Prosciutto sirloin pancetta pork pig rump, brisket biltong pastrami short loin leberkas doner tenderloin. Ham hock drumstick rump short ribs, sirloin ball tip pork belly boudin landjaeger ground round meatball.', comments: [{username: 'Ryan Williams', comment:'Can I do this?'}, {username: 'Bryan Lee', comment:'Yeah, man!'} ]},
+    {username: 'Anderson Von Quigley', img: 'img/male.png', link: 'Bacon ipsum dolor amet drumstick jerky venison tri-tip meatloaf swine leberkas bresaola frankfurter. Chicken ham hock ball tip leberkas corned beef. Frankfurter hamburger pig pastrami pork chop leberkas, filet mignon venison porchetta pancetta kielbasa tongue. ', comments: [{username: 'Ryan Williams', img: 'img/male.png', comment:'How do I register?'}]},
+    {username: 'Fred Bradley', img: 'img/male.png',link: 'Doner capicola chuck shoulder prosciutto tongue. Strip steak drumstick biltong chuck sirloin pancetta cow shank turducken tenderloin venison. Shankle t-bone bacon drumstick shoulder jowl frankfurter pancetta sausage tri-tip short loin.', comments: [{username: 'Ryan Williams', img: 'img/male.png', comment:'Can I do this?'}, {username: 'Bryan Lee', img: 'img/male.png', comment:'Yeah, man!'}, {username: 'Jackie Blue', img: 'img/fem.png', comment:'Nope.'}]},
+    {username: 'Jackie Ivanov', img: 'img/fem.png',link: 'Drumstick bacon beef sirloin, tri-tip porchetta pastrami.', comments: []},
+    {username: 'Anderson Von Quigley', img: 'img/male.png',link: 'Prosciutto sirloin pancetta pork pig rump, brisket biltong pastrami short loin leberkas doner tenderloin. Ham hock drumstick rump short ribs, sirloin ball tip pork belly boudin landjaeger ground round meatball.', comments: [{username: 'Ryan Williams', img: 'img/male.png', comment:'Can I do this?'}, {username: 'Bryan Lee', img: 'img/male.png', comment:'Yeah, man!'} ]},
   ];  
   this.closeallcomments = function() {
     $scope.closeallcomments = false;
@@ -15,9 +14,9 @@ app.controller('mainController', ['$scope', '$http', function($scope, $http) {
   };
   this.sumbitcomment = function(index, postcomment) {
     var eachcomment = this.feeddata[index].comments;
-    eachcomment.push({username: this.user, comment:postcomment});
+    eachcomment.push({username: this.user.name, img: this.user.img, comment:postcomment});
     postcomment = '';
-    debugger
+    // debugger
     //console.log(eachcomment)
   }
 }]);
