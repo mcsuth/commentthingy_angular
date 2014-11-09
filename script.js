26475b2418ab00ac425c74a993420ec2bd50ffdf
mcsuth <seimith@gmail.com>
2014-11-09 00:57:16 -0800

    Make routing... instead of single page

--- script.js
+++ script.js
@@ -1,22 +1,58 @@
-var app = angular.module('app', []);
+var app = angular.module('myApp', ['ngRoute'])
 
-app.controller('mainController', ['$scope', '$http', function($scope, $http) {
-  this.user = {name: "Smith Suth", img: 'img/fem.png'};
-  this.feeddata = [
-    {username: this.user.name, img: 'img/fem.png', link: 'Bacon ipsum dolor amet drumstick jerky venison tri-tip meatloaf swine leberkas bresaola frankfurter. Chicken ham hock ball tip leberkas corned beef. Frankfurter hamburger pig pastrami pork chop leberkas, filet mignon venison porchetta pancetta kielbasa tongue. ', comments: [{username: 'Ryan Williams', img: 'img/male.png', comment:'How do I register?'}]},
-    {username: 'Fred Bradley', img: 'img/male.png',link: 'Doner capicola chuck shoulder prosciutto tongue. Strip steak drumstick biltong chuck sirloin pancetta cow shank turducken tenderloin venison. Shankle t-bone bacon drumstick shoulder jowl frankfurter pancetta sausage tri-tip short loin.', comments: [{username: 'Ryan Williams', img: 'img/male.png', comment:'Can I do this?'}, {username: 'Bryan Lee', img: 'img/male.png', comment:'Yeah, man!'}, {username: 'Jackie Blue', img: 'img/fem.png', comment:'Nope.'}]},
+app.factory('factory', function() {
+  var currentUser = [{name: 'Smith Suth', img: 'img/fem.png'}];
+  var postIndexforcomment;
+  var mockdata = [
+    {username: currentUser[0].name, img: currentUser[0].img, link: 'Bacon ipsum dolor amet drumstick jerky venison tri-tip meatloaf swine leberkas bresaola frankfurter. Chicken ham hock ball tip leberkas corned beef. Frankfurter hamburger pig pastrami pork chop leberkas, filet mignon venison porchetta pancetta kielbasa tongue. ', comments: [{username: 'Ryan Williams', img: 'img/male.png', comment:'How do I register?'}]},
+    {username: 'Fred Bradley', img: 'img/male.png',link: 'Doner capicola chuck shoulder prosciutto tongue. Strip steak drumstick biltong chuck sirloin pancetta cow shank turducken tenderloin venison. Shankle t-bone bacon drumstick shoulder jowl frankfurter pancetta sausage tri-tip short loin.', comments: [{username: 'Ryan Williams', img: 'img/male.png', comment:'Can I do this?'}, {username: 'Bryan Lee', img: 'img/male.png', comment:'Yeah, man!'}, {username: 'Jackie Ivanov', img: 'img/fem.png', comment:'Nope.'}]},
     {username: 'Jackie Ivanov', img: 'img/fem.png',link: 'Drumstick bacon beef sirloin, tri-tip porchetta pastrami.', comments: []},
     {username: 'Anderson Von Quigley', img: 'img/male.png',link: 'Prosciutto sirloin pancetta pork pig rump, brisket biltong pastrami short loin leberkas doner tenderloin. Ham hock drumstick rump short ribs, sirloin ball tip pork belly boudin landjaeger ground round meatball.', comments: [{username: 'Ryan Williams', img: 'img/male.png', comment:'Can I do this?'}, {username: 'Bryan Lee', img: 'img/male.png', comment:'Yeah, man!'} ]},
-  ];  
-  this.closeallcomments = function() {
-    $scope.closeallcomments = false;
-    this.commcount = 0;
+  ]; 
+  var feeditems = {};
+  
+  feeditems.add = function(item) {
+    mockdata.push(item);
   };
-  this.sumbitcomment = function(index, postcomment) {
-    var eachcomment = this.feeddata[index].comments;
-    eachcomment.push({username: this.user.name, img: this.user.img, comment:postcomment});
-    postcomment = '';
-    // debugger
-    //console.log(eachcomment)
-  }
-}]);
+  feeditems.currentUser = function() {
+    return currentUser;
+  };
+  feeditems.list = function() {
+    return mockdata;
+  };
+  feeditems.setIndex = function(index) {
+    postIndexforcomment = index;
+  };
+  feeditems.postIndexforcomment = function() {
+    return postIndexforcomment;
+  };
+  return feeditems;
+});
+app.config(function($routeProvider) {
+    $routeProvider
+      // route for the home page
+      .when('/', {
+          templateUrl : 'pages/home.html',
+          controller  : 'mainController'
+      })
+      // route for the about page
+      .when('/addpost', {
+          templateUrl : 'pages/post.html',
+          controller  : 'addPostController'
+      })
+      .when('/addcomment', {
+          templateUrl : 'pages/comments.html',
+          controller  : 'addCommentController'
+      })
+    })
+function mainController($scope,factory) {
+    this.list = factory.list; 
+    this.currentUser = factory.currentUser;
+    this.whatindex = factory.setIndex;
+}
+function addPostController($scope, factory) {
+    this.add = factory.add;
+}
+function addCommentController($scope, factory) {
+    this.postIndexforcomment = factory.postIndexforcomment;
+}
