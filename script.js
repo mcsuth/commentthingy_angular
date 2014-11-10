81a6a4b5abadb51b2388a8b5380cecb92654369f
mcsuth <seimith@gmail.com>
2014-11-10 08:10:18 -0800

    Add date time to comments

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ app.factory('factory', function() {
   var postIndexforcomment;
   var commentIndex;
   var mockdata = [
-    {username: currentUser[0].username, img: currentUser[0].img, link: 'Bacon ipsum dolor amet drumstick jerky venison tri-tip meatloaf swine leberkas bresaola frankfurter. Chicken ham hock ball tip leberkas corned beef. Frankfurter hamburger pig pastrami pork chop leberkas, filet mignon venison porchetta pancetta kielbasa tongue. ', comments: [{username: 'Ryan', img: 'img/male.png', comment:'How do I register?'}]},
-    {username: 'Fred', img: 'img/male.png',link: 'Doner capicola chuck shoulder prosciutto tongue. Strip steak drumstick biltong chuck sirloin pancetta cow shank turducken tenderloin venison. Shankle t-bone bacon drumstick shoulder jowl frankfurter pancetta sausage tri-tip short loin.', comments: [{username: 'Ryan', img: 'img/male.png', comment:'Can I do this?'}, {username: currentUser[0].username, img: currentUser[0].img, comment:'Yeah, man!'}, {username: 'Jackie', img: 'img/fem.png', comment:'Nope.'}]},
-    {username: 'Jackie', img: 'img/fem.png',link: 'Drumstick bacon beef sirloin, tri-tip porchetta pastrami.', comments: []},
-    {username: 'Anderson', img: 'img/male.png',link: 'Prosciutto sirloin pancetta pork pig rump, brisket biltong pastrami short loin leberkas doner tenderloin. Ham hock drumstick rump short ribs, sirloin ball tip pork belly boudin landjaeger ground round meatball.', comments: [{username: 'Ryan', img: 'img/male.png', comment:'Can I do this?'}, {username: 'Bryan', img: 'img/male.png', comment:'Yeah, man!'} ]},
+    {username: currentUser[0].username, img: currentUser[0].img, time: 'Mon Nov 1 2014 08:03:54 GMT-0800 (PST)', link: 'Bacon ipsum dolor amet drumstick jerky venison tri-tip meatloaf swine leberkas bresaola frankfurter. Chicken ham hock ball tip leberkas corned beef. Frankfurter hamburger pig pastrami pork chop leberkas, filet mignon venison porchetta pancetta kielbasa tongue. ', comments: [{username: 'Ryan', img: 'img/male.png',time: 'Mon Nov 1 2014 08:03:54 GMT-0800 (PST)', comment:'How do I register?'}]},
+    {username: 'Fred', img: 'img/male.png', time: 'Mon Nov 4 2014 08:03:54 GMT-0800 (PST)', link: 'Doner capicola chuck shoulder prosciutto tongue. Strip steak drumstick biltong chuck sirloin pancetta cow shank turducken tenderloin venison. Shankle t-bone bacon drumstick shoulder jowl frankfurter pancetta sausage tri-tip short loin.', comments: [{username: 'Ryan', img: 'img/male.png',time: 'Mon Nov 1 2014 08:03:54 GMT-0800 (PST)', comment:'Can I do this?'}, {username: currentUser[0].username, img: currentUser[0].img, time: 'Mon Nov 1 2014 08:03:54 GMT-0800 (PST)', comment:'Yeah, man!'}, {username: 'Jackie', img: 'img/fem.png',time: 'Mon Nov 1 2014 08:03:54 GMT-0800 (PST)', comment:'Nope.'}]},
+    {username: 'Jackie', img: 'img/fem.png', time: 'Mon Nov 5 2014 08:03:54 GMT-0800 (PST)', link: 'Drumstick bacon beef sirloin, tri-tip porchetta pastrami.', comments: []},
+    {username: 'Anderson', img: 'img/male.png', time: 'Mon Nov 10 2014 08:03:54 GMT-0800 (PST)', link: 'Prosciutto sirloin pancetta pork pig rump, brisket biltong pastrami short loin leberkas doner tenderloin. Ham hock drumstick rump short ribs, sirloin ball tip pork belly boudin landjaeger ground round meatball.', comments: [{username: 'Ryan', img: 'img/male.png',time: 'Mon Nov 1 2014 08:03:54 GMT-0800 (PST)', comment:'Can I do this?'}, {username: 'Bryan', img: 'img/male.png',time: 'Mon Nov 1 2014 08:03:54 GMT-0800 (PST)', comment:'Yeah, man!'} ]},
   ]; 
   var feeditems = {};
   var itemComment;
@@ -29,8 +29,9 @@ app.factory('factory', function() {
     return postIndexforcomment;
   };
   feeditems.addComment = function(item) {
-    itemComment = {username: currentUser[0].username, img: currentUser[0].img, comment: item};
+    itemComment = {username: currentUser[0].username, img: currentUser[0].img, comment: item, time: new Date()};
     mockdata[postIndexforcomment].comments.push(itemComment);
+    debugger
   };
   feeditems.setCommentIndex = function(index) {
     commentIndex = index;
@@ -84,6 +85,7 @@ function mainController($scope, factory, $route, $routeParams, $location, $modal
     });
     this.modalitem = this.$index;
     this.setCommentIndex = factory.setCommentIndex(this.modalitem);
+    debugger
   };
 
 }
@@ -91,6 +93,7 @@ function addPostController($scope, factory) {
   this.add = factory.add;
 }
 function addCommentController($scope, factory, $rootScope){
+  this.blur = "blurbackground";
   $rootScope.$on('$routeChangeStart', function(event, currRoute, prevRoute){
     $rootScope.animation = currRoute.animation;
   });
